e25ba88ba28342741e504b1c24f45c2e5804257d
DuvanArwenLazar <3dtuningduvan@gmail.com>
2024-01-19 17:38:29 -0500

    Project Started

---
+++ src/components/navbar.vue
@@ -0,0 +1,25 @@
+<template class="content">
+    <div class="nav">
+        <a href="#" class="item"> Inicio </a>
+        <a href="#" class="item"> Asignaturas </a>
+        <a href="#" class="item"> Profesores Recomendados </a>
+        <a href="#" class="item"> UNAL Emprende </a>
+        <a href="#" class="item"> Lana Team </a>
+    </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .item {
+        color: #191717;
+        margin: 0 14px;
+        font-weight: 500;
+    }
+
+    .item:hover {
+        border-bottom: 3px solid #191717;
+    }
+</style>
